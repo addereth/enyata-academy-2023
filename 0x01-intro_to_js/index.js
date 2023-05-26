cd76001fbec95898a3fcb317dac6b4fda7c6cfa7
Seth Ayihi <juniorayihi@gmail.com>
2023-05-26 05:03:40 +0000

    Javascript

--- 0x01-intro_to_js/index.js
+++ 0x01-intro_to_js/index.js
@@ -9,15 +9,15 @@ myName = 'Ayihi';
 
 //Q4
 let car = {
-    CarBrand: 'Rolls Royce',
-    CarModel: 'Ghost',
-    CarType: 'luxury salon',
-    Wheels: 4,
-    Transmission: 'automatic',
-    SellingPrice: 100000,
-    Seats: 'leather',
-    EngineType: 'v6',
-    color: 'Ash',
-    condition: 'new',
+    CarBrand: 'Rolls Royce';
+    CarModel: 'Ghost';
+    CarType: 'luxury salon';
+    Wheels: 4;
+    Transmission: 'automatic';
+    SellingPrice: 100000;
+    Seats: 'leather';
+    EngineType: 'v6';
+    color: 'Ash';
+    condition: 'new';
 
 };
